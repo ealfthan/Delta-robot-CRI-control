d35ab0ffe91a34d0414dbff978e56783d59e642b
Elias <Elias@LAPTOP-RHAE91L2>
2026-06-09 10:01:30 +0300

    testing the din and dout functions

--- Delta robotin ohjaus/Program.cs
+++ Delta robotin ohjaus/Program.cs
@@ -4,14 +4,13 @@ using log4net;
 using static Delta_robotin_ohjaus.HardwareProtocolClient;
 
 
-double standardVel = 9999;
+double standardVel = 100;
 
 log4net.Config.BasicConfigurator.Configure();
 HardwareProtocolClient criController = new HardwareProtocolClient();
 
 criController.IPAddress = "localhost"; //192.168.3.11 || locahost
 criController.Port = 3921; // 3920 || 3921
-criController.flagHideBasicStatusMessages = false;
 
 criController.Connect();
 
@@ -21,17 +20,23 @@ if (!criController.WaitForConnection())
     return;
 }
 
+// a small wait should be added if accessing dout state right after setting it since the code might execute before irc has sent an update
+
 criController.SetDigitalOutput(22, true);
 criController.SetDigitalOutput(23, true);
+
+Thread.Sleep(2000);
 
 bool[] dout = criController.GetDigitalOutputs();
 
 for (int i = 0; i < dout.Length; i++)
 {
-    Console.WriteLine(dout[i] + ",");
+    Console.Write(dout[i] + ",");
 }
 
-// HUOM! 5mm < z < 135mm
+Console.Write("\n");
+
+// HUOM! 2mm < z < 138mm
 
 criController.MoveToCoord(motionType.Cart, 100, 0, 70, standardVel);
 if (!criController.WaitForMove()) { return; }
@@ -44,6 +49,10 @@ if (!criController.WaitForMove()) { return; }
 
 criController.MoveToCoord(motionType.Cart, 100, 0, 70, standardVel);
 if (!criController.WaitForMove()) { return; }
+
+criController.MoveToCoord(motionType.Cart, 0, 0, 138, standardVel);
+if (!criController.WaitForMove()) { return; }
+
 
 Thread.Sleep(5000);
 
